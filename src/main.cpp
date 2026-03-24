b39f010448644c9b8a637a29a4cbe97ecd961279
Caio Reis <fromcaio.contato@gmail.com>
2026-03-23 23:45:48 -0300

    First Commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    const std::string config_path = "config.json";
+
+    std::cout << "Loading config: " << config_path << std::endl;
+
+    return 0;
+}
